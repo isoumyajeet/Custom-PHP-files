a6e45399333b031c782fe9528ad02d9783a98c03
Soumyajeet Seal <soumyajeet009@gmail.com>
2017-05-27 23:44:39 +0530

    Add files via upload

---
+++ file-transfer-server-to-server.php
@@ -0,0 +1,13 @@
+<?php
+set_time_limit(0);
+
+
+$url = "http://www.infobot-technologies.com/demo/Aeropidia_ashu.zip";
+
+
+exec('wget http://www.infobot-technologies.com/demo/Aeropidia_ashu.zip');
+
+
+
+
+?>
