e5eb96a31811207ce34fa09500fdbe97a700135c
Soumyajeet Seal <soumyajeet009@gmail.com>
2019-02-13 16:52:08 +0530

    Update unzip-zip-file.php

--- unzip-zip-file.php
+++ unzip-zip-file.php
@@ -3,7 +3,9 @@
         
     $zip = new ZipArchive;
     if ($zip->open('filename-clean.zip') === TRUE) { // give the name of the zip file that you need to be unzip
-        $zip->extractTo(' /web/htdocs/www.targetsiteurl.com/home/targetfolder');// give the path name where the zip file need to be unzip
+        //$zip->extractTo(' /web/htdocs/www.targetsiteurl.com/home/targetfolder');// give the path name where the zip file need to be unzip
+        //OR
+        $zip->extractTo(dirname(__FILE__));
         $zip->close();
         echo 'ok';
     } else {
